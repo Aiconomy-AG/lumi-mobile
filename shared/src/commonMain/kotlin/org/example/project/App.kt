6ddc2d185c214ccd93643329790d6fe2286dc032
budasebastian <sebastianbuda2409@gmail.com>
2026-07-07 09:52:01 +0300

    commit - TaskListScreen component

--- shared/src/commonMain/kotlin/org/example/project/App.kt
+++ shared/src/commonMain/kotlin/org/example/project/App.kt
@@ -14,36 +14,19 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.resources.painterResource
 
 import kotlinproject.shared.generated.resources.Res
 import kotlinproject.shared.generated.resources.compose_multiplatform
+import org.example.project.presentation.tasks.TaskListScreen
+import org.example.project.presentation.tasks.TaskListViewModel
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        val viewModel = viewModel { TaskListViewModel() }
+        TaskListScreen(viewModel = viewModel)
     }
 }
