03e9e572b17a1990bc3414de02eafc8fd7d42bd9
Rares Fodorca <raresfodorca@Raress-MacBook-Air.local>
2026-07-08 13:52:03 +0300

    login with backend

--- shared/src/commonMain/kotlin/org/example/project/App.kt
+++ shared/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,26 +1,31 @@
 package org.example.project
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import features.main.MainScreen
-import org.example.project.data.auth.MockAuthRepository
+import org.example.project.data.ApiConfig
+import org.example.project.data.auth.AuthApiService
 import org.example.project.data.auth.UserSession
+import org.example.project.data.createHttpClient
 import org.example.project.presentation.auth.LoginScreen
 import org.example.project.presentation.auth.LoginViewModel
-import org.example.project.presentation.tasks.TaskListViewModel
 import org.example.project.presentation.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
     var currentUser by remember { mutableStateOf<UserSession?>(null) }
-    AppTheme {
 
+    AppTheme {
         if (currentUser == null) {
+            val httpClient = remember { createHttpClient() }
+
             val viewModel = remember {
                 LoginViewModel(
-                    authRepository = MockAuthRepository(),
+                    authRepository = AuthApiService(
+                        client = httpClient,
+                        baseUrl = ApiConfig.BASE_URL
+                    ),
                     onLoginSuccess = { user ->
                         currentUser = user
                     }
