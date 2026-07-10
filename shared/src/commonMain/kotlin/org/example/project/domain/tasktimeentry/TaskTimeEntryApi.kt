136beef957b1419f4500187d94ba73d0ceec65e1
Buda Sebastian <132143076+SebiB24@users.noreply.github.com>
2026-07-10 17:55:37 +0300

    Sync multiplatform clocking (#18)
    * commit - connect timer to live update with web sockets
    
    * commit - remove chat modification
    
    * commit - iOS live feedback fix

--- shared/src/commonMain/kotlin/org/example/project/domain/tasktimeentry/TaskTimeEntryApi.kt
+++ shared/src/commonMain/kotlin/org/example/project/domain/tasktimeentry/TaskTimeEntryApi.kt
@@ -2,6 +2,7 @@ package org.example.project.domain.tasktimeentry
 
 interface TaskTimeEntryApi {
     suspend fun getTimeEntries(taskId: Int): List<TaskTimeEntry>
+    suspend fun getActiveTimer(): TaskTimeEntry?
     suspend fun startTimer(taskId: Int): TaskTimeEntry
     suspend fun stopTimer(taskId: Int, entryId: Int): TaskTimeEntry
 }
